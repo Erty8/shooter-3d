8042150a6ac5b6aa3bdc1cb13dab28e074902793
Erty8 <erturulyavuzalp@gmail.com>
2018-07-31 18:21:34 +0300

    asteroid_random

---
+++ unity/Space Shooter/Assets/kodlar/oyunkontrol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oyunkontrol : MonoBehaviour {
+
+    public GameObject Asteroid;
+    public Vector3 randomPos;
+    public float baslangıcbekleme;
+    public float olusturmabekleme;
+    public float dongubekleme;
+	void Start () {
+        StartCoroutine (olustur());
+	}
+
+    IEnumerator olustur()
+    {
+        yield return new WaitForSeconds (baslangıcbekleme);
+        while (true)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Vector3 vec = new Vector3(Random.Range(-randomPos.x, randomPos.x), 0, randomPos.z);
+                Instantiate(Asteroid,vec, Quaternion.identity);
+                yield return new WaitForSeconds(olusturmabekleme);
+            }
+            yield return new WaitForSeconds(dongubekleme);
+        }
+    }
+}
--- unity/Space Shooter/Assets/kodlar/temaslayokol.cs
+++ unity/Space Shooter/Assets/kodlar/temaslayokol.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class temaslayokol : MonoBehaviour {
     public float sayac;
+	public GameObject patlama;
+	public GameObject player_patlama;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag!="sınır") {
@@ -13,10 +15,16 @@ public class temaslayokol : MonoBehaviour {
             if (sayac == 2)
             {
                 Destroy(gameObject);
+				Instantiate (patlama,transform.position,transform.rotation);
                 sayac = 0;
                 
 
             }
+
+        }
+        if (other.tag == "Player")
+        {
+            Instantiate(player_patlama, other.transform.position, other.transform.rotation);
         }
     }
 
